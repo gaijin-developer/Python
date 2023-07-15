9f12722c6269aaa0cc4d670018bce822b9449b6e
Frank Entsie <frank.entsie301@gmail.com>
2023-07-15 18:08:49 +0900

    methods, conditionals, builtins

---
+++ day 2/experiment.py
@@ -0,0 +1,13 @@
+# user_password = input("Please enter a password: ")
+#
+# while user_password != "password" :
+#     user_password = input("Please enter a password: ")
+#
+# print("password was correct")
+x = 1
+
+while x <= 10:
+    print(x)
+    x = x + 1
+
+print("password was correct")
