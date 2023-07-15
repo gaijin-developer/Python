0beb6a8b91e08ba576092ec18a241dbadee3cc16
Frank Entsie <frank.entsie301@gmail.com>
2023-07-15 17:16:17 +0900

    input and variables

---
+++ main.py
@@ -0,0 +1,9 @@
+question = 'Enter a to do:'
+todo1 = input(question)
+todo2 = input(question)
+todo3 = input(question)
+print(todo1)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(todos))
---
+++ title_length.py
@@ -0,0 +1,3 @@
+user_input = input("Please enter a  movie title: ")
+length = len(user_input)
+print("The number of characters in the movie '", user_input, "' is", length)
