367739baf41df16d3341163f3142c7403aa7b70a
Frank Entsie <frank.entsie301@gmail.com>
2023-07-19 13:14:06 +0900

    file reading and writing added

---
+++ Day 06/read_text_files.py
@@ -0,0 +1,29 @@
+while True:
+    user_input = input("Enter add, show, edit or complete : \n")
+
+    match user_input:
+        case 'add':
+            add_todo = input('Add a to do : \n') + "\n"
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            add_todo = add_todo.capitalize()
+            todos.append(add_todo)
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+        case 'show':
+            file = open('text.txt', 'r')
+            if len(todos) < 1:
+                number_of_todos = len(todos)
+                print(f'no todos available - {number_of_todos}')
+            else:
+                for i, todo in enumerate(todos):
+                    o_todo = f"{i + 1}.{todo}"
+                    print(o_todo)
+        case 'complete':
+            number = input("Enter input number of todo to complete: \n ")
+            number = int(number) - 1
+            todos.pop(number)
+        case 'edit':
+            print(todos)
+        case 'end' | 'exit':
+            break
